f5bc46e2cb55cc87d198e21f6431f933e2d70692
unknown <rudir.alena1011@gmail.com>
2023-03-24 20:25:33 +0200

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -4,6 +4,34 @@ const express = require('express')
 const router = express.Router()
 
 // ================================================================
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600 $ в місяць',
+  address: '51900,Дніпро, вул.Короленка,4-77',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'ivan@mail.com',
+      href: 'mailto:ivan@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
+//===================================================================
 
 // router.get Створює нам один ентпоїнт
 
@@ -23,10 +51,240 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Summary',
+    },
+    header,
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+        work on a new project I learn the domain and try to understand the idea of the project. Good team
+        player, every colleague is a friend to me.`,
+      },
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+          tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+          for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
   })
 })
 
 // ================================================================
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Skills',
+    },
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: '10',
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: '10',
+          isTop: true,
+        },
+        {
+          name: 'VS Code & Git',
+          point: '8',
+          isTop: true,
+        },
+        {
+          name: 'Terminal',
+          point: '9',
+        },
+        {
+          name: 'React.js',
+          point: '0',
+        },
+        {
+          name: 'PHP',
+          point: 'null',
+        },
+      ],
+      skills: [
+        {
+          name: 'HTML',
+          point: '10',
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: '10',
+          isTop: true,
+        },
+        {
+          name: 'VS Code & Git',
+          point: '8',
+          isTop: true,
+        },
+        {
+          name: 'Terminal',
+          point: '9',
+        },
+        {
+          name: 'React.js',
+          point: '0',
+        },
+        {
+          name: 'PHP',
+          point: 'null',
+        },
+      ],
+      hobbies: [
+        {
+          name: 'Догляд за конем',
+          isMain: true,
+        },
+        {
+          name: 'Східні єдиноборства',
+          isMain: true,
+        },
+        {
+          name: 'Вишивка',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Education',
+    },
+    header,
+    main: {
+      education: [
+        {
+          name: 'МАУП',
+          isEnd: false,
+        },
+        {
+          name: 'МНТУ',
+          isEnd: true,
+        },
+        {
+          name: 'IT STEP Universyty',
+          isEnd: false,
+        },
+        {
+          name: 'Університет економіки і права "КРОК"',
+          isEnd: true,
+        },
+      ],
+      certificates: [
+        {
+          name: 'fact',
+          id: 2,
+        },
+        {
+          name: 'argument',
+          id: 3,
+        },
+        {
+          name: 'poleno',
+          id: 5,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// Підключаємо роутер до бек-енду
+module.exports = router
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+    header,
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: null,
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmount: 3,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://resume.com.ua/',
+              about:
+                'VillageWaters is a three-year development project',
+              stacks: [
+                {
+                  name: 'React JS',
+                },
+                {
+                  name: 'HTML',
+                },
+                {
+                  name: 'Node JS',
+                },
+              ],
+              awards: [
+                {
+                  name: 'The LIFE Awards recognise the most innovative, inspirational and effective LIFE projects in three categories: nature protection, environment and climate action.',
+                },
+                {
+                  name: 'PMI Awards honor individuals and organizations whose passion and expertise make the greatest contributions to the project management profession.',
+                },
+              ],
+              stackAmount: 1,
+
+              awardAmount: 2,
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
+  })
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
