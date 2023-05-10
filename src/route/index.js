0207f49e463f3a0b05275286c201cd637d3f47a4
unknown <rudir.alena1011@gmail.com>
2023-05-10 14:28:10 +0300

    shophomeindex

--- src/route/index.js
+++ src/route/index.js
@@ -462,11 +462,9 @@ router.get('/work', function (req, res) {
   })
 })
 
-<<<<<<< HEAD
 router.get('/car', function (req, res) {
   // res.render генерує нам HTML сторінку
 
-  //            ↙ cюди вводимо назву файлу з сontainer
   res.render('car', {
     layout: 'default',
 
@@ -558,7 +556,9 @@ router.get('/car', function (req, res) {
       },
       total: 28990,
     },
-=======
+  })
+})
+
 //========================================================================
 
 router.get('/shophome', function (req, res) {
@@ -844,118 +844,5 @@ router.get('/shophome', function (req, res) {
         },
       ],
     ],
->>>>>>> shophome
-  })
-  //                  ↑↑ сюди вводимо JSON дані
-})
-
-<<<<<<< HEAD
-// ================================================================
-
-// Підключаємо роутер до бек-енду
-module.exports = router
-
-router.get('/person', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('person', {
-    layout: 'person',
-    person: {
-      name: 'Emma Johnson',
-      age: 32,
-      gender: 'Female',
-      address: {
-        street: '123 Main St',
-        city: 'New York',
-        state: 'NY',
-        zip: '10001',
-        country: 'USA',
-      },
-      education: [
-        {
-          degree: 'Bachelor of Science',
-          major: 'Computer Science',
-          university:
-            'Massachusetts Institute of Technology',
-          graduationYear: 2012,
-        },
-      ],
-      workExperience: [
-        {
-          company: 'Google',
-          title: 'Software Engineer',
-          startDate: '2012-06-01',
-          endDate: '2016-12-31',
-          responsibilities: [
-            'Developed new features for Google Maps',
-            'Worked on improving search algorithms',
-          ],
-          year_founded: 1990,
-          industry: 'Technology',
-          employees: [
-            {
-              name: 'John Smith',
-              position: 'CEO',
-              department: 'Executive',
-              projects: [
-                {
-                  name: 'Project Alpha',
-                  description:
-                    'Developing new software platform',
-                  status: 'In Progress',
-                  teams: [
-                    {
-                      team_name: 'Awesome Team',
-                      team_leader: {
-                        name: 'John Smith',
-                        title: 'Team Leader',
-                        email: 'john.smith@example.com',
-                      },
-                      team_members: [
-                        {
-                          name: 'Alice Johnson',
-                          title: 'Software Engineer',
-                          email:
-                            'alice.johnson@example.com',
-                          skills: ['Java', 'Python', 'SQL'],
-                          projects: [
-                            {
-                              name: 'Project A',
-                              description:
-                                'Lorem ipsum dolor sit amet',
-                              technologies: [
-                                'Java',
-                                'Spring Framework',
-                              ],
-                              team_members: [
-                                {
-                                  name: 'Bob Lee',
-                                  title:
-                                    'Software Engineer',
-                                },
-                                {
-                                  name: 'Cindy Chen',
-                                  title: 'UI Designer',
-                                },
-                              ],
-                            },
-                          ],
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
   })
 })
-
-// ================================================================
-
-=======
->>>>>>> shophome
-// Підключаємо роутер до бек-енду
-module.exports = router
